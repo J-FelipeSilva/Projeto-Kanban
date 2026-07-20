e48c3a2988f96adfd7e24f8ef5e5721e7a1c391b
J-FelipeSilva <joao.felipesilva@outlook.com>
2026-07-20 16:20:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function dragstartHandler(event) {
 	event.dataTransfer.setData("text", event.target.id);
 }
+
 function dragoverHandler(event) {
 	event.preventDefault();
 }
+
 function dropHandler(event) {
 	event.preventDefault();
 	const info = event.dataTransfer.getData("text");
@@ -11,4 +13,42 @@ function dropHandler(event) {
 	if (localCerto) {
 		localCerto.appendChild(document.getElementById(info));
 	}
+}
+
+function criarTarefa(buttonElement) {
+	//recebe o texto da tarefa
+	const texto = prompt("Descreva a tarefa");
+	if (texto===null || texto.trim()=="") {
+		return;
+	}
+	//cria os elementos necessários do DOM html
+	const newDiv = document.createElement("div");
+	const newSpan = document.createElement("span");
+	newDiv.id = "tarefa-" + Date.now();
+	newDiv.className = "post";
+	newDiv.setAttribute("draggable", "true");
+	newDiv.setAttribute("ondragstart", "dragstartHandler(event)");
+	newSpan.innerText = texto;
+	newDiv.appendChild(newSpan);
+	//adiciona na coluna
+	const coluna = buttonElement.closest(".coluna");
+	if (coluna) {
+		const colunaCerta = coluna.querySelector(".post-its");
+		colunaCerta.appendChild(newDiv);
+	}
+}
+
+function criarFluxoDeTrabalho() {
+	//recebe o título da Etapa de Trabalho
+	const texto = prompt("Título");
+	if (texto===null || texto.trim()=="") {
+		return;
+	}
+	
+	const quadro = document.getElementById("quadro");
+	const newColuna = document.createElement("div");
+	const newSpan = document.createElement("span");
+	
+	newColuna.id = "coluna-" + Date.now();
+	newColuna.className = "coluna";
 }
